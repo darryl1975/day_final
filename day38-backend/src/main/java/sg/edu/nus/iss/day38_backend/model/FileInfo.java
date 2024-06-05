51ac403865c23823135e89130ba3e30c6e9e2dfc
darryl1975 <darryl1975@hotmail.com>
2024-06-05 13:50:24 +0800

    FileInfo.java

--- day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/model/FileInfo.java
+++ day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/model/FileInfo.java
@@ -1,5 +1,31 @@
 package sg.edu.nus.iss.day38_backend.model;
 
 public class FileInfo {
+ 
+    String filename;
+
+    String fileURL;
+    
+    public FileInfo(String filename, String fileURL) {
+        this.filename = filename;
+        this.fileURL = fileURL;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFileURL() {
+        return fileURL;
+    }
+
+    public void setFileURL(String fileURL) {
+        this.fileURL = fileURL;
+    }
+
     
 }
