738236b55248e088de2d7aa32b1dc7114ee22873
darryl1975 <darryl1975@hotmail.com>
2024-06-05 10:06:20 +0800

    initial commit

---
+++ day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/config/S3Config.java
@@ -0,0 +1,36 @@
+package sg.edu.nus.iss.day38_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class S3Config {
+
+    @Value("${s3.access.key}")
+    private String S3_Access_Key;
+
+    @Value("${s3.secret.key}")
+    private String S3_Secret_Key;
+
+    // exclude bucket named day39 in EndpointConfiguration
+    // https://day39.sgp1.digitaloceanspaces.com
+    @Bean
+    public AmazonS3 getS3Client() {
+        BasicAWSCredentials cred = new BasicAWSCredentials(
+                S3_Access_Key, S3_Secret_Key);
+        EndpointConfiguration epConfig = new EndpointConfiguration(
+                "sgp1.digitaloceanspaces.com", "sgp1");
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(epConfig)
+                .withCredentials(new AWSStaticCredentialsProvider(cred))
+                .build();
+    }
+}
---
+++ day38-backend/src/test/java/sg/edu/nus/iss/day38_backend/Day38BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day38_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day38BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
