67ae80f0baa1a1b6aa83ae3c544794c7befd953d
darryl1975 <darryl1975@hotmail.com>
2024-06-05 11:57:47 +0800

    added create directoory in main()

--- day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/Day38BackendApplication.java
+++ day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/Day38BackendApplication.java
@@ -1,13 +1,26 @@
 package sg.edu.nus.iss.day38_backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import sg.edu.nus.iss.day38_backend.service.ImageService;
+
 @SpringBootApplication
-public class Day38BackendApplication {
+public class Day38BackendApplication implements CommandLineRunner {
+
+	@Autowired
+	ImageService imgSvc;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day38BackendApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		imgSvc.initDirectory();
 	}
 
 }
