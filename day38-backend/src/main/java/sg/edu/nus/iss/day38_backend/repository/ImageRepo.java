ce42f0a7a861c5e38f9b35c24773f37413a392bd
darryl1975 <darryl1975@hotmail.com>
2024-06-05 11:13:26 +0800

    added getPicture and save funciton in ImageRepo

--- day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/repository/ImageRepo.java
+++ day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/repository/ImageRepo.java
@@ -1,8 +1,14 @@
 package sg.edu.nus.iss.day38_backend.repository;
 
+import java.io.InputStream;
+import java.sql.ResultSet;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import sg.edu.nus.iss.day38_backend.model.ImageData;
 
 @Repository
 public class ImageRepo {
@@ -14,4 +20,23 @@ public class ImageRepo {
     public static final String SQL_INSERT_PICTURE = "insert into pictures (pic_id, content, mime) VALUES (?, ?, ?)";
 
     public static final String SQL_GET_PICTURE_BY_ID = "select * from pictures where pic_id = ?";
+
+    public void save(String pic_id, InputStream is, String contentType) {
+        jdbcTemplate.update(SQL_INSERT_PICTURE, pic_id, is, contentType);
+    }
+
+    public Optional<ImageData> getPicture(String pic_id) {
+
+        return jdbcTemplate.query(SQL_GET_PICTURE_BY_ID, (ResultSet rs) -> {
+
+            // there is no record found
+            if (!rs.next())
+                return Optional.empty();
+            
+            ImageData data = new ImageData(rs.getString("pic_id"), rs.getBytes("content"), rs.getString("mine"));
+            return Optional.of(data);
+        }, pic_id);
+    } 
+
+    
 }
---
+++ day38-backend/src/main/java/sg/edu/nus/iss/day38_backend/service/ImageService.java
@@ -0,0 +1,5 @@
+package sg.edu.nus.iss.day38_backend.service;
+
+public class ImageService {
+    
+}
